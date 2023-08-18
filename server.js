96b3ffba58d77f96ad2c7eaaf42ab2ca6885ea77
befierce <enterbhatt123@gmail.com>
2023-08-18 09:42:42 +0530

    Added express.js package

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const http = require('http');
+const express = require('express');
 
-const routes = require('./route');
+const app = express();
 
-console.log(routes.someText)
-const server = http.createServer(routes.handler);
+// Middleware function
+app.use((req, res, next) => {
+    console.log('print1');
+     // Don't forget to call next() to pass control to the next middleware
+    res.send('<h1> hello from 1st middleware </h1>');
+    next();
+});
 
+app.use((req, res, next) => {
+    console.log('print2')
+});
 
-server.listen(4000);
+const server = http.createServer(app);
+
+server.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
