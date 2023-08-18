cd707c4a07317da578028e1f595161b03019fa76
befierce <enterbhatt123@gmail.com>
2023-08-18 13:11:02 +0530

    used middlewares and did routing

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');//importing the body parser
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:false}));
+
 // Middleware function
-app.use((req, res, next) => {
-    console.log('print1');
-     // Don't forget to call next() to pass control to the next middleware
-    res.send('<h1> hello from 1st middleware </h1>');
-    next();
+app.use('/add-product',(req, res, next) => {
+    //console.log('in 1st middleware');
+    res.send('<form action ="/product" method = "POST"><input type = "text" name = "title"><input type = "text" name = "size"><button type = "submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('print2')
+app.post('/product',(req, res, next) => {
+    console.log(req.body)//body needs to be parsed
+    res.redirect('/');
 });
+app.use('/',(req, res, next)=>{
+    res.send('<h1>Hello From Express</h1>')
+})
 
-const server = http.createServer(app);
 
-server.listen(4000, () => {
-    console.log('Server is running on port 4000');
-});
+
+// const server = http.createServer(app);
+
+// server.listen(4000, () => {
+//     console.log('Server is running on port 4000');
+// });
+app.listen(4000);
