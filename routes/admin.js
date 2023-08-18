f6366d187ae8fa4f034ba917ab3fabfcdbfa446c
befierce <enterbhatt123@gmail.com>
2023-08-18 15:00:21 +0530

    added err page not found and made url funnel

--- routes/admin.js
+++ routes/admin.js
@@ -3,15 +3,17 @@ const express = require('express');//importing express
 
 const router = express.Router(); //calling router which is the functionality of express js
 
+
+//admin/add-product=> get Request
 router.get('/add-product',(req, res, next) => {
     //console.log('in 1st middleware');
-    res.send('<form action ="/product" method = "POST"><input type = "text" name = "title"><input type = "text" name = "size"><button type = "submit">Add Product</button></form>');
+    res.send('<form action ="/admin/add-product" method = "POST"><input type = "text" name = "title"><input type = "text" name = "size"><button type = "submit">Add Product</button></form>');
 });
-
-router.post('/product',(req, res, next) => {
+//admin/add-product => POST request
+router.post('/add-product',(req, res, next) => {
     console.log(req.body)//body needs to be parsed
-    res.redirect('/');
+    res.redirect('/shop');
 });
 
 
-module.exports = router;//router will be a middleware function
+module.exports = router;//router will be a middle
